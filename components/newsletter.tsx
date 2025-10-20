b15e74074a0e17d116ad6743663acff26a205617
Faseeh06 <faseehsafdar06@gmail.com>
2025-10-20 19:26:11 +0500

    Change in Message

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -168,7 +168,7 @@ export const Newsletter = () => {
                 }}
                 className="text-base short:lg:text-lg sm:text-lg lg:text-xl !leading-[1.1] font-medium text-center text-foreground text-pretty"
               >
-                Explore our partner businesses and connect with us!
+                Explore our partner sponsors and connect with us!
               </motion.p>
             </div>
           </motion.div>
